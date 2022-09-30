de810e0791308f58d6da76c697aeb3af4fdcd4b0
Annie Wu <anniew@andrew.cmu.edu>
2022-09-30 19:40:41 -0400

    added unit test cases for database changes and modified dao to resolve bug

--- docs-core/src/test/java/com/sismics/docs/core/dao/jpa/TestJpa.java
+++ docs-core/src/test/java/com/sismics/docs/core/dao/jpa/TestJpa.java
@@ -1,7 +1,9 @@
 package com.sismics.docs.core.dao.jpa;
 
 import com.sismics.docs.BaseTransactionalTest;
+import com.sismics.docs.core.dao.DocumentDao;
 import com.sismics.docs.core.dao.UserDao;
+import com.sismics.docs.core.model.jpa.Document;
 import com.sismics.docs.core.model.jpa.User;
 import com.sismics.docs.core.util.TransactionUtil;
 import com.sismics.docs.core.util.authentication.InternalAuthenticationHandler;
@@ -25,6 +27,15 @@ public class TestJpa extends BaseTransactionalTest {
         user.setRoleId("admin");
         user.setStorageQuota(10L);
         String id = userDao.create(user, "me");
+
+        // Create a new document
+        DocumentDao documentDao = new DocumentDao(); 
+        Document document = new Document();
+        document.setTitle("test1");
+        document.setAvg_tech("10");
+        document.setAvg_interpersonal("9");
+        document.setAvg_fit("8");
+        document.setNum_reviews(0);
         
         TransactionUtil.commit();
 
@@ -32,6 +43,14 @@ public class TestJpa extends BaseTransactionalTest {
         user = userDao.getById(id);
         Assert.assertNotNull(user);
         Assert.assertEquals("toto@docs.com", user.getEmail());
+
+        // Search a document by user ID
+        document = documentDao.findByUserId(id).get(0);
+        Assert.assertNotNull(document);
+        Assert.assertEquals("10", document.getAvg_tech());
+        Assert.assertEquals("9", document.getAvg_interpersonal());
+        Assert.assertEquals("8", document.getAvg_fit());
+        Assert.assertTrue(0 == document.getNum_reviews());
 
         // Authenticate using the database
         Assert.assertNotNull(new InternalAuthenticationHandler().authenticate("username", "12345678"));
