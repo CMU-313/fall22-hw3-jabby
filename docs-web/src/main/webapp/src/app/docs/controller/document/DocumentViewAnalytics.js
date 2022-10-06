c6119becee804256558d01643ea46c71820d92b0
Brandon Wang <brandonwang2000@gmail.com>
2022-10-06 12:55:22 -0400

    use RatingResource API to fetch percentage of reviewers

--- docs-web/src/main/webapp/src/app/docs/controller/document/DocumentViewAnalytics.js
+++ docs-web/src/main/webapp/src/app/docs/controller/document/DocumentViewAnalytics.js
@@ -56,8 +56,12 @@
      * Get percentage of reviews and average ratings from server
      */
     $scope.loadReviews = function () {
+        Restangular.one('rate', $stateParams.id).get().then(function (data) {
+            $scope.num_reviews = data.percentage_rating; 
+        });
+        
         Restangular.one('document', $stateParams.id).get().then(function (data) {
-            $scope.ratings = [data.num_reviews, data.tech_rating, data.interpersonal_rating, data.fit_rating];
+            $scope.ratings = [$scope.num_reviews, data.tech_rating, data.interpersonal_rating, data.fit_rating];
             // $scope.ratings = [4/7 * 100, 8.8, 2.0, 0.0]; // purely for testing
             $scope.styleReviews();
         }, function (response) {
